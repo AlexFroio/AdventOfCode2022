349dfbbcd257e6724687e62b1332a6cb268a2698
Alex Froio <alexfroio43@gmail.com>
2022-12-23 21:44:32 -0500

    opened day 5 and made some comments

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -9,10 +9,12 @@ fn count_the_char_from_mult_bags (c: Chunk<Lines>) -> i32 {
     let mut res:i32 = ch as i32;
 
     if res >= b'a' as i32 {
+        // subtract the 'a' character's value from our character and start over from 1
         res -= b'a' as i32;
         res += 1;
     }
     else if res >= b'A' as i32 {
+        // subtract the 'a' character's value from our character and start over from 27
         res -= b'A' as i32;
         res += 27;
     }
@@ -44,11 +46,13 @@ fn count_the_char (s: &str) -> i32 {
 }
 
 fn compare_all_3_bags (mut c: Chunk<Lines>) -> char {
+    // Takes 3 chunks of lines and tries to find a common item among the bags.
     let mut item = '[';
     let bag_1 = c.next().unwrap();
     let bag_2 = c.next().unwrap();
     let bag_3 = c.next().unwrap();
 
+    // Compare bags 1 and 2 first. Come out of the comparison with a full list of matches.
     let mut matched_1_2: Vec<char> = Vec::new();
     for ch_1 in bag_1.chars() {
         for ch_2 in bag_2.chars() {
@@ -57,6 +61,7 @@ fn compare_all_3_bags (mut c: Chunk<Lines>) -> char {
             }
         }
     }
+    // Iterate over the matched list from bags 1 and 2 for 3. Credit for limiting the list goes to DEX for breaking me out of a funk
     for ch_n in matched_1_2 {
         for ch_3 in bag_3.chars() {
             if ch_n == ch_3 {
@@ -64,7 +69,8 @@ fn compare_all_3_bags (mut c: Chunk<Lines>) -> char {
             }
         }
     }
-
+    // tbh I am annoyed at how not scalable this is. Every bag increases code size by one double for loop.
+    // Also it kinda all assumes we will always have a match between all n bags, so we have no case for no matches.
 
     return item
 }
--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,21 +1,32 @@
 use common_utils;
 
 fn is_this_duty_overlap (s:&str) -> bool {
-
+    // This function's logic was flipped once the problem became needing to report
+    // any overlap at all. 
     let ranges = split_str_into_duties(s);
     let mut inside = false;
     for i in ranges[0]..=ranges[1] {
         print!("{} ", i);
         if (ranges[2]..(ranges[3] + 1)).contains(&i) { 
+            // Initially, for part 1, this was empty. We didn't want to flip a switch unless
+            // we knew we had exited the set.
             inside = true 
         }
         else {
+            //For part 1, inside started as True, would get set to false on exiting the set.
             //inside = false;
         }
     }
     println!("{}", inside);
+    // For part 1, this was just `if inside` and then we would reset inside to true afterwards
+    // I am annoyed at myself for having 2 return points but this was the easiest way to return a true
+    // with how I worked out my pattern and design logic.
     if !inside {
         return inside;
+        // If you're wondering why I'm annoyed, it's because in C Safety programming, multiple return paths from a 
+        // function are considered unsafe design and no code should exist without explicit need to serve some sort of
+        // effect, side effect or affect. AKA if you can prove the latter half of this is dead code, it should be 
+        // reworked.
     }
     inside = false; 
     for i in ranges[2]..=ranges[3] {
